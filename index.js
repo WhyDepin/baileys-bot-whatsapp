6807affc79e70f1598a1868572b99942312b2a67
WhyDepin <fxdeveloper417@gmail.com>
2023-10-20 17:50:32 +0700

    lose

---
+++ index.js
@@ -0,0 +1,228 @@
+const { Boom } = require('@hapi/boom')
+const NodeCache = require('node-cache')
+const readline = require('readline')
+const { makeWASocket, AnyMessageContent, delay, DisconnectReason, fetchLatestBaileysVersion, getAggregateVotesInPollMessage, makeCacheableSignalKeyStore, makeInMemoryStore, PHONENUMBER_MCC, proto, useMultiFileAuthState, WAMessageContent, WAMessageKey } = require('@whiskeysockets/baileys')
+const open = require('open')
+const fs = require('fs')
+const Pino = require('pino')
+const chalk = require('chalk')
+const { parsePhoneNumber } = require("libphonenumber-js")
+const config = require("./config.json")
+const { Client, Serialize } = require("./lib/serialize.js")
+
+const logger = Pino({ level: "fatal" }).child({ level: "fatal" })
+
+function nocache(module, cb = () => { }) {
+	console.log(`${module} menunggu informasi..`) 
+	fs.watchFile(require.resolve(module), async () => {
+		await uncache(require.resolve(module))
+		cb(module)
+	})
+}
+
+function uncache(module = '.') {
+	return new Promise((resolve, reject) => {
+		try {
+			delete require.cache[require.resolve(module)]
+			resolve()
+		} catch (e) {
+			reject(e)
+		}
+	})
+}
+
+const usePairingCode = !!config.pairingNum || process.argv.includes('--use-pairing-code')
+const useMobile = process.argv.includes('--mobile')
+
+const store = makeInMemoryStore({ logger: Pino({ level: "fatal" }).child({ level: "fatal" }) })
+
+setInterval(() => {
+	store.writeToFile('./bjir_deck.json')
+}, 30000)
+
+//membaca line
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
+const question = (text) => new Promise((resolve) => rl.question(text, resolve))
+
+// memulai koneksi
+const startSock = async() => {
+	const { state, saveCreds } = await useMultiFileAuthState('./bjir_deck')
+	const msgRetryCounterCache = new NodeCache()
+
+	const sock = makeWASocket({
+	  logger: Pino({ level: "fatal" }).child({ level: "fatal" }), 
+	  printQRInTerminal: !usePairingCode,
+      mobile: useMobile,
+      auth: {
+         creds: state.creds,
+         keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
+      },
+      browser: ['Chrome (Linux)', '', ''],
+      markOnlineOnConnect: true,
+      generateHighQualityLinkPreview: true,
+      getMessage: async (key) => {
+         let jid = jidNormalizedUser(key.remoteJid)
+         let msg = await store.loadMessage(jid, key.id)
+         return msg?.message || ""
+      },
+      msgRetryCounterCache, 
+      defaultQueryTimeoutMs: undefined,
+	})
+
+	store.bind(sock.ev)
+	
+
+   await Client({ sock, store })
+	
+	// konek menggunakan pairing code
+	if (usePairingCode && !sock.authState.creds.registered) {
+      if (useMobile) throw new Error('Cannot use pairing code with mobile api')
+	
+let phoneNumber
+      if (!!config.pairingNum) {
+         phoneNumber = config.pairingNum.replace(/[^0-9]/g, '')
+
+         if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
+            console.log(chalk.bgBlack(chalk.redBright("Start with your country's WhatsApp code, Example : 62xxx")))
+            process.exit(0)
+         }
+      } else {
+         phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number : `)))
+         phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
+
+         // Ask again when entering the wrong number
+         if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
+            console.log(chalk.bgBlack(chalk.redBright("Start with your country's WhatsApp code, Example : 62xxx")))
+
+            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number : `)))
+            phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
+            rl.close()
+         }
+      }
+
+      setTimeout(async () => {
+         let code = await sock.requestPairingCode(phoneNumber)
+         code = code?.match(/.{1,4}/g)?.join("-") || code
+         console.log(chalk.black(chalk.bgBlue(`Your Pairing Code : `)), chalk.black(chalk.white(code)))
+      }, 3000)
+   }
+		
+
+	// mobile koneksi
+	if(useMobile && !sock.authState.creds.registered) {
+		const { registration } = sock.authState.creds || { registration: {} }
+
+		if(!registration.phoneNumber) {
+			         let phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number : `)))
+         phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
+		
+		if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
+            console.log(chalk.bgBlack(chalk.redBright("Start with your country's WhatsApp code, Example : 62xxx")))
+
+            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number : `)))
+            phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
+         }
+
+         registration.phoneNumber = "+" + phoneNumber
+      }
+
+		const phoneNumber = parsePhoneNumber(registration.phoneNumber)
+		if(!phoneNumber?.isValid()) {
+			throw new Error('Invalid phone number: ' + registration.phoneNumber)
+		}
+
+		registration.phoneNumber = phoneNumber.format('E.164')
+		registration.phoneNumberCountryCode = phoneNumber.countryCallingCode
+		registration.phoneNumberNationalNumber = phoneNumber.nationalNumber
+		const mcc = PHONENUMBER_MCC[phoneNumber.countryCallingCode]
+		registration.phoneNumberMobileCountryCode = mcc
+
+		async function enterCode() {
+			try {
+				const code = await question('Please enter the one time code:\n')
+				const response = await sock.register(code.replace(/["']/g, '').trim().toLowerCase())
+				console.log('Successfully registered your phone number.')
+				console.log(response)
+				rl.close()
+			} catch(error) {
+				console.error('Failed to register your phone number. Please try again.\n', error)
+				await askForOTP()
+			}
+		}
+
+		async function enterCaptcha() {
+			const response = await sock.requestRegistrationCode({ ...registration, method: 'captcha' })
+			const path = __dirname + '/captcha.png'
+			fs.writeFileSync(path, Buffer.from(response.image_blob, 'base64'))
+
+			open(path)
+			const code = await question('Please enter the captcha code:\n')
+			fs.unlinkSync(path)
+			registration.captcha = code.replace(/["']/g, '').trim().toLowerCase()
+		}
+async function askOTP() {
+         if (!registration.method) {
+            let code = await question(chalk.bgBlack(chalk.greenBright('What method do you want to use? "sms" or "voice" : ')))
+            code = code.replace(/["']/g, '').trim().toLowerCase()
+
+            if (code !== 'sms' && code !== 'voice') return await askOTP()
+
+            registration.method = code
+         }
+
+         try {
+            await sock.requestRegistrationCode(registration)
+            await enterCode()
+         } catch (e) {
+            console.error('Failed to request registration code. Please try again.\n', e)
+            if (e?.reason === 'code_checkpoint') {
+               await enterCaptcha()
+            }
+            await askOTP()
+         }
+      }
+
+      await askOTP()
+   }
+   
+   // write session
+   sock.ev.on("creds.update", saveCreds)
+   
+   //update no restart
+   nocache('./message/mess', module => console.log(chalk.greenBright('[ WHATSAPP BOT ]  ') + chalk.cyanBright(` "${module}" Telah diupdate!`)))
+   nocache('./lib/serialize', module => console.log(chalk.greenBright('[ WHATSAPP BOT ]  ') + chalk.cyanBright(` "${module}" Telah diupdate!`)))
+   
+  sock.multi = true
+	sock.nopref = false
+	sock.prefa = 'anjing'
+      sock.ev.on("messages.upsert", async (message) => {
+      if (!message.messages) return
+      const m = await Serialize(sock, message.messages[0])
+      await (await import(`./message/mess.js?v=${Date.now()}`)).default(sock, m, store, message)
+   })
+   
+       sock.ev.on("connection.update", async (update) => {
+      const { lastDisconnect, connection, qr } = update
+      if (connection) {
+         console.info(`Connection Status : ${connection}`)
+      }
+
+      if (connection === "close") {
+         let reason = new Boom(lastDisconnect?.error)?.output.statusCode
+   if (reason === DisconnectReason.connectionClosed) {
+            console.log("Connection closed, reconnecting....")
+            await startSock()
+         } else if (reason === DisconnectReason.connectionLost) {
+            console.log("Connection Lost from Server, reconnecting...")
+            await startSock()
+         } else if (reason === DisconnectReason.restartRequired) {
+            console.log("Restart Required, Restarting...")
+            await startSock()
+         } 
+      }
+   })
+
+    return sock
+  }
+
+startSock()
---
+++ message/mess.js
@@ -0,0 +1,184 @@
+// akumah masih pemula
+// setting in config.json
+/*
+• Team Tabrak Lurus
+• WhyDepin-Darwin-KiiCode
+*/
+const {
+	downloadContentFromMessage,
+	generateWAMessage,
+	generateWAMessageFromContent,
+	generateProfilePicture
+} = require("@whiskeysockets/baileys")
+const baileys = require("@whiskeysockets/baileys")
+const fs = require("fs")
+const chalk = require("chalk")
+const axios = require("axios")
+const path = require("path")
+const { exec } = require("child_process")
+const util = require("util")
+var config = require("../config.json")
+const func = require("../lib/function.js")
+
+//singkat
+let evalOwn = ['6283896302781@s.whatsapp.net', '6283877118785@s.whatsapp.net']
+var icon = fs.readFileSync("./src/dep.jpg")
+
+module.exports = async(sock, m, store) => {
+try {
+    if (!m) return
+   if (m.isBaileys) return
+ 
+const content = JSON.stringify(m.message)
+const from = m.key.remoteJid
+const chats = (m.type === 'conversation' && m.message.conversation) ? m.message.conversation : (m.type == 'imageMessage') && m.message.imageMessage.caption ? m.message.imageMessage.caption : (m.type == 'documentMessage') && m.message.documentMessage.caption ? m.message.documentMessage.caption : (m.type == 'videoMessage') && m.message.videoMessage.caption ? m.message.videoMessage.caption : (m.type == 'extendedTextMessage') && m.message.extendedTextMessage.text ? m.message.extendedTextMessage.text : (m.type == 'buttonsResponseMessage' && m.message.buttonsResponseMessage.selectedButtonId) ? m.message.buttonsResponseMessage.selectedButtonId : (m.type == 'templateButtonReplyMessage') && m.message.templateButtonReplyMessage.selectedId ? m.message.templateButtonReplyMessage.selectedId : '' 
+if (sock.multi) {
+var prefix = /^[°•π÷×¶∆£¢€¥®™✓_=|~!?#$%^&.+-,\/\\©^]/.test(chats) ? chats.match(/^[°•π÷×¶∆£¢€¥®™✓_=|~!?#$%^&.+-,\/\\©^]/gi) : '#'
+} else {
+if (sock.nopref) {
+prefix = ''
+} else {
+prefix = sock.prefa
+}
+}
+const args = chats.split(' ')
+const command = chats.toLowerCase().split(' ')[0] || ''
+const isCmd = command.startsWith(prefix)
+const quoted = m.isQuoted ? m.quoted : m
+const q = chats.slice(command.length + 1, chats.length)
+const botNumber = sock.user.id.split(':')[0] + '@s.whatsapp.net'
+const isGroup = m.key.remoteJid.endsWith('@g.us')
+const sender = isGroup ? (m.key.participant ? m.key.participant : m.participant) : m.key.remoteJid
+const isOwner = config.owner == sender ? true : [`${config.owner}@s.whatsapp.net`].includes(sender) ? true : false
+const groupMetadata = isGroup ? await sock.groupMetadata(from) : ''
+const groupName = isGroup ? groupMetadata.subject : ''
+const groupId = isGroup ? groupMetadata.id : ''
+const groupMembers = isGroup ? groupMetadata.participants : ''
+const groupAdmins = isGroup ? getGroupAdmins(groupMembers) : ''
+const ppgc = await conn.profilePictureUrl(from, 'image')
+const ppuser = await conn.profilePictureUrl(sender, 'image')
+const isBotGroupAdmins = groupAdmins.includes(botNumber) || false
+const isGroupAdmins = groupAdmins.includes(sender)
+        
+//baris function
+const reply = (teks) => {
+			sock.sendMessage(from, { text: teks }, { quoted: m })
+	}
+const sendContact = (jid, numbers, name, quoted, mn) => {
+let number = numbers.replace(/[^0-9]/g, '')
+const vcard = 'BEGIN:VCARD\n' 
+			+ 'VERSION:3.0\n' 
+			+ 'FN:' + name + '\n'
+			+ 'ORG:;\n'
+			+ 'TEL;type=CELL;type=VOICE;waid=' + number + ':+' + number + '\n'
+			+ 'END:VCARD'
+			return sock.sendMessage(from, { contacts: { displayName: name, contacts: [{ vcard }] }, mentions : mn ? mn : []},{ quoted: m })
+		}
+const sendMess = (hehe, teks) => {
+	sock.sendMessage(hehe, { text: teks })
+}
+
+const sendText = (from, teks) => {
+	sock.sendMessage(from, { text: teks })
+}
+
+if (chats.startsWith("=> ") && isOwner && evalOwn) {
+		console.log(chalk.green('[EVAL]'), chalk.white(`Dari Owner aowkoakwoak`))
+		  const ev = (sul) => {
+            var sat = JSON.stringify(sul, null, 2)
+            var bang = util.format(sat)
+            if (sat == undefined) {
+              bang = util.format(sul)
+            }
+            return reply(bang)
+          }
+          try {
+           reply(util.format(eval(`;(async () => { ${chats.slice(2)} })()`)))
+          } catch (e) {
+           reply(util.format(e))
+          }
+		} else if (chats.startsWith("$ ") && isOwner) {
+        console.log(chalk.green('[EXEC]'), chalk.white(`Dari Owner aowkoakwoak`))
+          exec(chats.slice(2), (err, stdout) => {
+		    if (err) return reply(`${err}`)
+		    if (stdout) reply(`${stdout}`)
+		  })
+        } else if (chats.startsWith("> ") && isOwner && evalOwn) {
+	    console.log(chalk.green('[EVAL]'), chalk.white(`Dari Owner aowkaokwoak`))
+		 try {
+	       let evaled = await eval(chats.slice(2))
+		   if (typeof evaled !== 'string') evaled = require("util").inspect(evaled)
+			reply(`${evaled}`)
+		 } catch (err) {
+		   reply(`${err}`)
+		 }
+		}
+
+// log chat masuk
+if (m.message && !m.isBaileys) {
+console.log(chalk.white(chalk.bgBlue("DARI :")), chalk.white(m.pushName), chalk.black(chalk.yellow(sender)) + "\n" + chalk.white(chalk.bgBlue("DI :")), chalk.black(chalk.bgWhite(isGroup ? groupMetadata.subject : "Private Chat", m.from)) + "\n" + chalk.white(chalk.bgBlue("PESAN :")), chalk.black(chalk.bgWhite(m.body || m.type)))
+        }
+            
+switch (command) {
+
+case prefix+"menu": case prefix+"help": {
+                let text = `
+_*>Hello User<*_
+_Bot ini masih dalam perkembangan, wajar jika fitur sedikit_
+
+┌─────────────────────
+│ _*Name:*_ _${m.pushName}_
+│ _*Number:*_ _${sender.split('@')[0]}_
+├─────────────────────
+│ _*Bot Name:*_ _Bens - MD_
+│ _*Created By:*_ _Team Tabrak Lurus_
+└─────────────────────
+
+_*-MAIN MENU-*_
+${prefix}owner
+${prefix}sc
+${prefix}menfes
+
+_*-OWNER MENU-*_
+${prefix}broadcast
+> (Eval)
+$ (Exec)`
+
+sock.sendMessage(from, {text: text, contextInfo: { externalAdReply: {  title: 'Bens - MD', body: '© Team Tabrak Lurus', thumbnail: fs.readFileSync("./src/dep.jpg"), sourceUrl: 'https://youtube.com/playlist?list=RDwjWmfnvIrDw&playnext=1&si=MgY1RAkwmw43y80B', mediaType: 1, showAdAttribution: true, renderLargerThumbnail: true }}}, {quoted: m})
+}
+break
+case prefix+'sc': {
+  reply('_*Coming soon In Github Or Youtube*_')
+  break
+}
+ case prefix+"owner": {
+   sendContact(from, `${config.owner}@s.whatsapp.net`, 'WhyDepin', m)
+ }
+ break
+ 
+case prefix+'bc': case prefix+'broadcast':
+if (!isOwner) return reply(config.msg.owner)
+if (args.length < 2) return reply(`Masukkan isi pesannya`)
+var data = await store.chats.all()
+ for (let i of data) {
+ sock.sendMessage(i.id, { text: `_*🚨Broadcast🚨*_\n\n${q}` })
+await func.sleep(1000)
+ }
+break
+
+case prefix+'menfes': case prefix+'confes': {
+if (!q) return reply('Example : ${prefix + command} 62xxx|nama|pesan')
+y = q.split('|')[0]
+b = q.split('|')[1]
+n = q.split('|')[2]
+let txt = `_Hai, ada secret message nih_\nDari: _*${b}*_\nPesan: _*${n}*_`
+sock.sendMessage(`${y}@s.whatsapp.net`, {text: txt}, {quoted:m})
+reply('success')
+} 
+break
+
+}
+ } catch (e) {
+        console.log(util.format(e))
+    }
+}
