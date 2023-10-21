a920e03863e7f7b49d4db5d7d663a7a688a70688
FXdeveloper <88582191+WhyDepin@users.noreply.github.com>
2023-10-21 17:40:01 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+exports.apikey = "apikey"
+exports.namabot = 'Bens - MD'
+exports.owner = ["628871746203"]
+exports.prefix =   "/^[°•π÷×¶∆£¢€¥®™+✓_=|/~!?@#%^&.©^]/i"
+exports.pairingNum = ""
