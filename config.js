37a7caf46b77fe5b552ff39694d02a7750971e7c
FXdeveloper <88582191+WhyDepin@users.noreply.github.com>
2023-10-22 05:36:40 +0700

    update config.js

--- config.js
+++ config.js
@@ -1,8 +1,16 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-exports.apikey = "apikey"
-exports.namabot = 'Bens - MD'
-exports.owner = ["628871746203"]
-exports.prefix =   "/^[°•π÷×¶∆£¢€¥®™+✓_=|/~!?@#%^&.©^]/i"
-exports.pairingNum = ""
+global.apikey = "apikey"//setting apikey
+global.namabot = 'Bens - MD'//setting bot name
+global.owner = ["628871746203"]//settinh owner
+global.prefix =   "/^[°•π÷×¶∆£¢€¥®™+✓_=|/~!?@#%^&.©^]/i"
+global.pairingNum = ""//contoh: 62xxxx
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.yellow(`'${__filename}' telah di update`))
+    delete require.cache[file]
+    require(file)
+})
